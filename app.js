e9461f9d4be4eaad1378ad2e531fb875fc856959
Sydney Smith <sydmac@gmail.com>
2016-05-15 16:55:51 -0400

    set up new mongolab account

--- app.js
+++ app.js
@@ -33,6 +33,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
+app.use(methodOverride(function(req, res){
+ if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+   // look in urlencoded POST bodies and delete it
+   var method = req.body._method;
+   delete req.body._method;
+   return method;
+ }
+}));
 
 app.get('/sign_s3', function(req, res){
     aws.config.update({accessKeyId: AWS_ACCESS_KEY , secretAccessKey: AWS_SECRET_KEY });
