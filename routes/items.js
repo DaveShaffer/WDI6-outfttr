d753025de87c431d20529ef72fa3fc915065ae9c
Sydney Smith <sydmac@gmail.com>
2016-05-16 15:41:20 -0400

    added authentication for item router

--- routes/items.js
+++ routes/items.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var Item = require('../models/item.js')
+var Item = require('../models/item')
 
 function makeError(res, message, status) {
     res.statusCode = status;
@@ -9,19 +9,24 @@ function makeError(res, message, status) {
     return error;
 }
 
+function authenticate(req, res, next) {
+    if (!req.isAuthenticated()) {
+        res.redirect('/');
+    } else {
+        next();
+    }
+}
+
 // INDEX
-router.get('/', function(req, res, next) {
+router.get('/', authenticate, function(req, res, next) {
     // get all the todos and render the index view
-    Item.find({})
-        .then(function(items) {
-            res.render('items/index', { items: items });
-        }, function(err) {
-            return next(err);
-        });
+    var items = global.currentUser.items;
+    console.log('currentUser', global.currentUser);
+    res.render('items/index', { items: items });
 });
 
 // NEW
-router.get('/new', function(req, res, next) {
+router.get('/new', authenticate, function(req, res, next) {
     var item = {
         name: '',
         type: '',
@@ -34,27 +39,24 @@ router.get('/new', function(req, res, next) {
 });
 
 // SHOW
-router.get('/:id', function(req, res, next) {
-    Item.findById(req.params.id)
-        .then(function(item) {
-            if (!item) return next(makeError(res, 'Document not found', 404));
-            res.render('items/show', { item: item });
-        }, function(err) {
-            return next(err);
-        });
+router.get('/:id', authenticate, function(req, res, next) {
+    var item = currentUser.items.id(req.params.id);
+    if(!item) return next(makeError(res, 'Document not found', 404));
+    res.render('items/show', { item: item });
 });
 
 // CREATE
-router.post('/', function(req, res, next) {
-    var item = new Item({
+router.post('/', authenticate, function(req, res, next) {
+    var item = {
         name: req.body.name,
         type: req.body.type,
         avatar_url: req.body.avatar_url,
         isClean: req.body.completed ? true : false,
         color: req.body.color,
         material: req.body.material
-    });
-    item.save()
+    };
+    currentUser.items.push(item);
+    currentUser.save()
         .then(function() {
             res.redirect('/items');
         }, function(err) {
@@ -63,29 +65,39 @@ router.post('/', function(req, res, next) {
 });
 
 // EDIT
-router.get('/:id/edit', function(req, res, next) {
-    Item.findById(req.params.id)
-        .then(function(item) {
-            if (!item) return next(makeError(res, 'Document not found', 404));
-            res.render('items/edit', { item: item });
-        }, function(err) {
-            return next(err);
-        });
+router.get('/:id/edit', authenticate, function(req, res, next) {
+    var item = currentUser.items.id(req.params.id);
+    if(!item) return next(makeError(res, 'Document not found', 404));
+    res.render('items/edit', { item: item });
 });
 
 // UPDATE
-router.put('/:id', function(req, res, next) {
-    Item.findById(req.params.id)
-        .then(function(item) {
-            if (!item) return next(makeError(res, 'Document not found', 404));
+router.put('/:id', authenticate, function(req, res, next) {
+    var item = currentUse.items.id(req.params.id);
+    if(!item) return next(makeError(res, 'Document not found', 404));
+    else{
             item.name = req.body.name;
             item.type = req.body.type;
             item.avatar_url = req.body.avatar_url;
             isClean = req.body.completed ? true : false;
             color: req.body.color;
             material: req.body.material;
-            return item.save();
-        })
+            currentUser.save()
+            .then(function(saved){
+                res.redirect('/items');
+            }, function(err){
+                return next(err)
+            });
+    }
+});
+
+// DESTROY
+router.delete('/:id', authenticate, function(req, res, next) {
+    var item = currentUser.items.id(req.params.id);
+    if (!item) return next(makeError(res, 'Document not found', 404));
+    var index = currentUser.items.indexOf(item);
+    currentUser.items.splice(index, 1);
+    currentUser.save()
         .then(function(saved) {
             res.redirect('/items');
         }, function(err) {
@@ -93,29 +105,19 @@ router.put('/:id', function(req, res, next) {
         });
 });
 
-// DESTROY
-router.delete('/:id', function(req, res, next) {
-    Item.findByIdAndRemove(req.params.id)
-        .then(function() {
-            res.redirect('/items');
-        }, function(err) {
-            return next(err);
-        });
-});
-
 // TOGGLE completed
-router.get('/:id/toggle', function(req, res, next) {
-    Item.findById(req.params.id)
-        .then(function(item) {
-            if (!item) return next(makeError(res, 'Document not found', 404));
-                item.lastWorn = !item.lastWorn;
-                item.save()
-                    .then(function(saved) {
-                        res.redirect('/items');
-                    }, function(err) {
-                        return next(err);
-                    });
-        });
+router.get('/:id/toggle', authenticate, function(req, res, next) {
+    var item = currentUser.items.findById(req.params.id);
+    if (!item) return next(makeError(res, 'Document not found', 404));
+    else {
+        item.lastWorn = !item.lastWorn;
+        item.save()
+            .then(function(saved) {
+                res.redirect('/items');
+            }, function(err) {
+                return next(err);
+            });
+        }ƒ
 });
 
 module.exports = router;
