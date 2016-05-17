0c5e753976524fd896468a47cc799d337d5923bc
Sydney Smith <sydmac@gmail.com>
2016-05-17 12:22:01 -0400

    added views for outfit and routing for outfit

--- routes/outfits.js
+++ routes/outfits.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
 var Outfit = require('../models/outfit');
+var Item = require('../models/item');
 
 function makeError(res, message, status) {
   res.statusCode = status;
@@ -21,72 +21,46 @@ function authenticate(req, res, next) {
 
 // INDEX
 router.get('/', authenticate, function(req, res, next) {
-  var todos = global.currentUser.todos;
-  res.render('outfits/index', { outfits: outfits, message: req.flash() });
+  var outfits = global.currentUser.outfits;
+  res.render('outfits/index', { outfits: outfits });
 });
 
-// NEW
 router.get('/new', authenticate, function(req, res, next) {
-  var todo = {
-    title: '',
-    completed: false
-  };
-  res.render('outfits/new', { outfit: outfit, message: req.flash() });
-});
+    var outfit = {
 
+    };
+    res.render('items/new', { item: item });
+});
 
 /// SHOW
 router.get('/:id', authenticate, function(req, res, next) {
   var outfit = currentUser.outfits.id(req.params.id);
   if (!outfit) return next(makeError(res, 'Document not found', 404));
-  res.render('outfits/show', { outfit: outfit, message: req.flash() } );
+  res.render('outfits/show', { outfit: outfit } );
 });
 
 // CREATE
 router.post('/', authenticate, function(req, res, next) {
-  var outfit = {
-    title: req.body.title,
-    completed: req.body.completed ? true : false
-  };
-  // Since a user's outfits are an embedded document, we just need to push a new
-  // Outfit to the user's list of outfits and save the user.
-  currentUser.outfits.push(item);
-  currentUser.save()
-  .then(function() {
-    res.redirect('/outfits');
-  }, function(err) {
-    return next(err);
-  });
-});
+    var outfit = {
 
-// EDIT
-router.get('/:id/edit', authenticate, function(req, res, next) {
-  var outfit = currentUser.outfits.id(req.params.id);
-  if (!outfit) return next(makeError(res, 'Document not found', 404));
-  res.render('outfits/edit', { outfit: outfit, message: req.flash() } );
-});
-
-// UPDATE
-router.put('/:id', authenticate, function(req, res, next) {
-  var outfit = currentUser.outfits.id(req.params.id);
-  if (!outfit) return next(makeError(res, 'Document not found', 404));
-  else {
-    outfit.title = req.body.title;
-    outfit.completed = req.body.completed ? true : false;
+    };
+    var items = global.currentUser.items;
+    var item = currentUser.items.id(req.params.id);
+    currentUser.outfit.items.push(item);
     currentUser.save()
-    .then(function(saved) {
-      res.redirect('/todos');
-    }, function(err) {
-      return next(err);
-    });
-  }
+        .then(function() {
+            res.redirect('/outfits');
+        }, function(err) {
+            return next(err);
+        });
 });
+
 
 // DESTROY
 router.delete('/:id', authenticate, function(req, res, next) {
   var outfit = currentUser.outfits.id(req.params.id);
   if (!outfit) return next(makeError(res, 'Document not found', 404));
-  var index = currentUser.outfits.indexOf(outfits);
+  var index = currentUser.outfits.indexOf(outfit);
   currentUser.outfits.splice(index, 1);
   currentUser.save()
   .then(function(saved) {
