5b101d4d0b6db2f996c92feb641af696828cee5e
D Shaffer <404shaff@gmail.com>
2016-05-19 11:08:29 -0400

    api is working

--- public/javascripts/wx.js
+++ public/javascripts/wx.js
@@ -1,4 +1,4 @@
-
+// $(document).ready(function() {
     // convert temperatures from degrees Kelvin to degress Fahrenheit
     function tempConv(x) {
       return Math.round((x - 273.15) * 1.8 + 32);
@@ -8,11 +8,10 @@ window.getWeather = function() {
 
     var getIP = 'http://ip-api.com/json/';
     $.getJSON(getIP).done(function(location) {
-      // var wxKey = '97096dd9f52a61051e6abc4495f2d176';
-      var wxKey = process.env.WX_KEY;
+      var wxKey = '97096dd9f52a61051e6abc4495f2d176';
+      // wxKey = process.env.WX_KEY;
       var wxUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + location.lat + '&lon=' + location.lon + '&appid=' + wxKey;
       // get uset's weather forecast
-      $('#currentLocation').text('location: ' + location.city);
       $.getJSON(wxUrl, function(data) {
         // show current conditions
         $('#currentTemp').text('Current conditions in  ' + location.city + ', ' + location.region + ': ' + Math.round(tempConv(data.list[0].main.temp)) + 'F and ' + data.list[0].weather[0].description);
@@ -35,3 +34,4 @@ window.getWeather = function() {
       }); // end weather JSON
     }); // end location JSON
   }
+// });
